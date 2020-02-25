e58ca0fd0bd15489d4b37bca2449bf36598c3c53
Masumi.M <masumi.lacrosse@live.jp>
2020-02-25 15:52:50 +0900

    Remove: Comments

--- app.py
+++ app.py
@@ -2,20 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# # ルートにおいて、index.htmlをレンダリングさせる
 @app.route('/')
 def index():
     _title = "Auto-Generator of Template Message"
     _messageFile = {"title": "Title", "desc": "Description"}
     return render_template('index.html', title=_title, messageFile=_messageFile)
 
-# # exchangeMessageというリクエスト受けた時に、実行する関数
 @app.route('/exchangeMessage', methods=['GET', 'POST'])
 def exchangeMessage():
     _title = "Auto-Generator of Template Message"
     if request.method == 'GET':
         print('GET Request')
-        # res = request.args.get('title_input')
     elif request.method == 'POST':
         print('POST Request')
         print(request.form['title_input'])
@@ -26,8 +23,6 @@ def exchangeMessage():
 
     return render_template("index.html", title=_title, messageFile=_messageFile)
 
-
-# # Localhostを立てるためのコマンド
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8080)
